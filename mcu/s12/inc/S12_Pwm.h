d85c8e64695d3825edd5942919583ea4e1099db8
Christoph Schueler <cpu12.gems@googlemail.com>
2011-06-22 11:59:57 +0200

    Added Functions to S12 PWM-Driver.

--- mcu/s12/inc/S12_Pwm.h
+++ mcu/s12/inc/S12_Pwm.h
@@ -1,8 +1,8 @@
 /*
- * k_dk - Driver Kit for k_os (Konnex Operating-System based on the 
+ * k_dk - Driver Kit for k_os (Konnex Operating-System based on the
  * OSEK/VDX-Standard).
  *
- * (C) 2007-2010 by Christoph Schueler <github.com/Christoph2,
+ * (C) 2007-2011 by Christoph Schueler <github.com/Christoph2,
  *                                      cpu12.gems@googlemail.com>
  *
  * All Rights Reserved
@@ -23,7 +23,7 @@
  *
  */
 #if !defined(__S12_PWM_H)
-#define	__S12_PWM_H
+#define __S12_PWM_H
 
 #include "S12_Hw.h"
 
@@ -35,144 +35,149 @@ extern "C"
 /*
 **  Register-Offsets.
 */
-#define PWME            ((uint8)0x00)
-    /*  PWME-Bits.  */
-    #define PWME7       ((uint8)0x80)
-    #define PWME6       ((uint8)0x40)
-    #define PWME5       ((uint8)0x20)
-    #define PWME4       ((uint8)0x10)
-    #define PWME3       ((uint8)0x08)
-    #define PWME2       ((uint8)0x04)
-    #define PWME1       ((uint8)0x02)
-    #define PWME0       ((uint8)0x01)
+#define PWME                    ((uint8)0x00)
+/*  PWME-Bits.  */
+    #define PWME7               ((uint8)0x80)
+    #define PWME6               ((uint8)0x40)
+    #define PWME5               ((uint8)0x20)
+    #define PWME4               ((uint8)0x10)
+    #define PWME3               ((uint8)0x08)
+    #define PWME2               ((uint8)0x04)
+    #define PWME1               ((uint8)0x02)
+    #define PWME0               ((uint8)0x01)
 
-#define PWMPOL          ((uint8)0x01)
-    /*  PWMPOL-Bits.  */
-    #define PPOL7       ((uint8)0x80)
-    #define PPOL6       ((uint8)0x40)
-    #define PPOL5       ((uint8)0x20)
-    #define PPOL4       ((uint8)0x10)
-    #define PPOL3       ((uint8)0x08)
-    #define PPOL2       ((uint8)0x04)
-    #define PPOL1       ((uint8)0x02)
-    #define PPOL0       ((uint8)0x01)
+#define PWMPOL                  ((uint8)0x01)
+/*  PWMPOL-Bits.  */
+    #define PPOL7               ((uint8)0x80)
+    #define PPOL6               ((uint8)0x40)
+    #define PPOL5               ((uint8)0x20)
+    #define PPOL4               ((uint8)0x10)
+    #define PPOL3               ((uint8)0x08)
+    #define PPOL2               ((uint8)0x04)
+    #define PPOL1               ((uint8)0x02)
+    #define PPOL0               ((uint8)0x01)
 
-#define PWMCLK          ((uint8)0x02)
-    /*  PWMCLK-Bits.  */
-    #define PCLK7       ((uint8)0x80)
-    #define PCLK6       ((uint8)0x40)
-    #define PCLK5       ((uint8)0x20)
-    #define PCLK4       ((uint8)0x10)
-    #define PCLK3       ((uint8)0x08)
-    #define PCLK2       ((uint8)0x04)
-    #define PCLK1       ((uint8)0x02)
-    #define PCLK0       ((uint8)0x01)
+#define PWMCLK                  ((uint8)0x02)
+/*  PWMCLK-Bits.  */
+    #define PCLK7               ((uint8)0x80)
+    #define PCLK6               ((uint8)0x40)
+    #define PCLK5               ((uint8)0x20)
+    #define PCLK4               ((uint8)0x10)
+    #define PCLK3               ((uint8)0x08)
+    #define PCLK2               ((uint8)0x04)
+    #define PCLK1               ((uint8)0x02)
+    #define PCLK0               ((uint8)0x01)
 
-#define PWMPRCLK        ((uint8)0x03)
-    /*  PWMPRCLK-Bits.  */
-    #define PCKB2       ((uint8)0x40)
-    #define PCKB1       ((uint8)0x20)
-    #define PCKB0       ((uint8)0x10)
-    #define PCKA2       ((uint8)0x04)
-    #define PCKA1       ((uint8)0x02)
-    #define PCKA0       ((uint8)0x01)
+#define PWMPRCLK                ((uint8)0x03)
+/*  PWMPRCLK-Bits.  */
+    #define PCKB2               ((uint8)0x40)
+    #define PCKB1               ((uint8)0x20)
+    #define PCKB0               ((uint8)0x10)
+    #define PCKA2               ((uint8)0x04)
+    #define PCKA1               ((uint8)0x02)
+    #define PCKA0               ((uint8)0x01)
 
-#define PWMCAE          ((uint8)0x04)
-    /*  PWMCAE-Bits.  */
-    #define CAE7        ((uint8)0x80)
-    #define CAE6        ((uint8)0x40)
-    #define CAE5        ((uint8)0x20)
-    #define CAE4        ((uint8)0x10)
-    #define CAE3        ((uint8)0x08)
-    #define CAE2        ((uint8)0x04)
-    #define CAE1        ((uint8)0x02)
-    #define CAE0        ((uint8)0x01)
+#define PWMCAE                  ((uint8)0x04)
+/*  PWMCAE-Bits.  */
+    #define CAE7                ((uint8)0x80)
+    #define CAE6                ((uint8)0x40)
+    #define CAE5                ((uint8)0x20)
+    #define CAE4                ((uint8)0x10)
+    #define CAE3                ((uint8)0x08)
+    #define CAE2                ((uint8)0x04)
+    #define CAE1                ((uint8)0x02)
+    #define CAE0                ((uint8)0x01)
 
-#define PWMCTL          ((uint8)0x05)
-    /*  PWMCTL-Bits.  */
-    #define CON67       ((uint8)0x80)
-    #define CON45       ((uint8)0x40)
-    #define CON23       ((uint8)0x20)
-    #define CON01       ((uint8)0x10)
-    #define PSWAI       ((uint8)0x08)
-    #define PFRZ        ((uint8)0x04)
+#define PWMCTL                  ((uint8)0x05)
+/*  PWMCTL-Bits.  */
+    #define CON67               ((uint8)0x80)
+    #define CON45               ((uint8)0x40)
+    #define CON23               ((uint8)0x20)
+    #define CON01               ((uint8)0x10)
+    #define PSWAI               ((uint8)0x08)
+    #define PFRZ                ((uint8)0x04)
 
-#define PWMTST          ((uint8)0x06)
+#define PWMTST                  ((uint8)0x06)
 
-#define PWMPRSC         ((uint8)0x07)
+#define PWMPRSC                 ((uint8)0x07)
 
-#define PWMSCLA         ((uint8)0x08)
-    /* 0 ==> Presc. = 512; 1 ==> Presc. = 2 */
-#define PWMSCLB         ((uint8)0x09)
+#define PWMSCLA                 ((uint8)0x08)
+/* 0 ==> Presc. = 512; 1 ==> Presc. = 2 */
+#define PWMSCLB                 ((uint8)0x09)
 
-#define PWMSCNTA        ((uint8)0x0A)
+#define PWMSCNTA                ((uint8)0x0A)
 
-#define PWMSCNTB        ((uint8)0x0B)
+#define PWMSCNTB                ((uint8)0x0B)
 
-#define PWMCNT0         ((uint8)0x0C)
+#define PWMCNT0                 ((uint8)0x0C)
 
-#define PWMCNT1         ((uint8)0x0D)
+#define PWMCNT1                 ((uint8)0x0D)
 
-#define PWMCNT2         ((uint8)0x0E)
+#define PWMCNT2                 ((uint8)0x0E)
 
-#define PWMCNT3         ((uint8)0x0F)
+#define PWMCNT3                 ((uint8)0x0F)
 
-#define PWMCNT4         ((uint8)0x10)
+#define PWMCNT4                 ((uint8)0x10)
 
-#define PWMCNT5         ((uint8)0x11)
+#define PWMCNT5                 ((uint8)0x11)
 
-#define PWMCNT6         ((uint8)0x12)
+#define PWMCNT6                 ((uint8)0x12)
 
-#define PWMCNT7         ((uint8)0x13)
+#define PWMCNT7                 ((uint8)0x13)
 
-#define PWMPER0         ((uint8)0x14)
-#define PWMPER1		((uint8)0x15)
-#define PWMPER01        ((uint8)0x14)
+#define PWMPER0                 ((uint8)0x14)
+#define PWMPER1                 ((uint8)0x15)
+#define PWMPER01                ((uint8)0x14)
 
-#define PWMPER2         ((uint8)0x16)
-#define PWMPER3         ((uint8)0x17)
-#define PWMPER23        ((uint8)0x16)
+#define PWMPER2                 ((uint8)0x16)
+#define PWMPER3                 ((uint8)0x17)
+#define PWMPER23                ((uint8)0x16)
 
-#define PWMPER4         ((uint8)0x18)
-#define PWMPER5         ((uint8)0x19)
-#define PWMPER45        ((uint8)0x18)
+#define PWMPER4                 ((uint8)0x18)
+#define PWMPER5                 ((uint8)0x19)
+#define PWMPER45                ((uint8)0x18)
 
-#define PWMPER6         ((uint8)0x1A)
-#define PWMPER7         ((uint8)0x1B)
-#define PWMPER67        ((uint8)0x1A)
+#define PWMPER6                 ((uint8)0x1A)
+#define PWMPER7                 ((uint8)0x1B)
+#define PWMPER67                ((uint8)0x1A)
 
-#define PWMDTY0         ((uint8)0x1C)
-#define PWMDTY1         ((uint8)0x1D)
-#define PWMDTY01        ((uint8)0x1C)
+#define PWMDTY0                 ((uint8)0x1C)
+#define PWMDTY1                 ((uint8)0x1D)
+#define PWMDTY01                ((uint8)0x1C)
 
-#define PWMDTY2         ((uint8)0x1E)
-#define PWMDTY3         ((uint8)0x1F)
-#define PWMDTY23        ((uint8)0x1E)
+#define PWMDTY2                 ((uint8)0x1E)
+#define PWMDTY3                 ((uint8)0x1F)
+#define PWMDTY23                ((uint8)0x1E)
 
-#define PWMDTY4         ((uint8)0x20)
-#define PWMDTY5         ((uint8)0x21)
-#define PWMDTY45        ((uint8)0x20)
+#define PWMDTY4                 ((uint8)0x20)
+#define PWMDTY5                 ((uint8)0x21)
+#define PWMDTY45                ((uint8)0x20)
 
-#define PWMDTY6         ((uint8)0x22)
-#define PWMDTY7         ((uint8)0x23)
-#define PWMDTY67        ((uint8)0x22)
+#define PWMDTY6                 ((uint8)0x22)
+#define PWMDTY7                 ((uint8)0x23)
+#define PWMDTY67                ((uint8)0x22)
 
-#define PWMSDN          ((uint8)0x24)
-    /*  PWMSDN-Bits.  */
-    #define PWMIF       ((uint8)0x80)
-    #define PWMIE       ((uint8)0x40)
-    #define PWMRSTRT    ((uint8)0x20)
-    #define PWMLVL      ((uint8)0x10)
-    #define PWM7IN      ((uint8)0x04)
-    #define PWM7INL     ((uint8)0x02)
-    #define PWM7ENA     ((uint8)0x01)
+#define PWMSDN                  ((uint8)0x24)
+/*  PWMSDN-Bits.  */
+    #define PWMIF               ((uint8)0x80)
+    #define PWMIE               ((uint8)0x40)
+    #define PWMRSTRT            ((uint8)0x20)
+    #define PWMLVL              ((uint8)0x10)
+    #define PWM7IN              ((uint8)0x04)
+    #define PWM7INL             ((uint8)0x02)
+    #define PWM7ENA             ((uint8)0x01)
 /*
 **  0x25-0x27 Reserved.
 */
 
-
 #define S12PWM8B8C_NUM_CHANNELS ((uint8)8)
 
+
+/*
+**  Global Types.
+*/
+typedef uint8   S12Pwm_ChannelType;
+typedef uint16  S12Pwm_PeriodType;
 
 typedef enum tagS12Pwm_StatusType {
     S12PWM_OK,
@@ -180,43 +185,63 @@ typedef enum tagS12Pwm_StatusType {
     S12PWM_ID
 } S12Pwm_StatusType;
 
-
 typedef struct tagS12Pwm_ConfigType {
     uint16 BaseAddr;
 
-    uint8 PwmE;
-    uint8 PwmCtl;
-    uint8 PwmPol;
-    uint8 PwmClk;
-    uint8 PwmPrClk;
-    uint8 PwmCae;
-    uint8 PwmSclA;
-    uint8 PwmSclB;
+    uint8   PwmE;
+    uint8   PwmCtl;
+    uint8   PwmPol;
+    uint8   PwmClk;
+    uint8   PwmPrClk;
+    uint8   PwmCae;
+    uint8   PwmSclA;
+    uint8   PwmSclB;
+    uint8   PwmSdn;
+    uint16  PwmPer01;
+    uint16  PwmPer23;
+    uint16  PwmPer45;
+    uint16  PwmPer67;
+    uint16  PwmDty01;
+    uint16  PwmDty23;
+    uint16  PwmDty45;
+    uint16  PwmDty67;
 
-    uint8 PwmSdn;
-
-    uint16 PwmPer01;
-    uint16 PwmPer23;
-    uint16 PwmPer45;
-    uint16 PwmPer67;
-
-    uint16 PwmDty01;
-    uint16 PwmDty23;
-    uint16 PwmDty45;
-    uint16 PwmDty67;
+    uint8 IdleState;
 } S12Pwm_ConfigType;
 
 
-void S12Pwm_Init(void);
+/*
+**  Global Variables.
+*/
+extern S12Pwm_ConfigType const * PWM;
 
-void S12Pwm_SetTimebase(void);
-void S12Pwm_SetDutyCycle(uint8 ChannelNumber,uint16 DutyCycle);
-void S12Pwm_SetPeriodAndDuty(uint8 ChannelNumber,uint16 Period,uint16 DutyCycle);
-void S12Pwm_SetOutputToIdle(uint8 ChannelNumber);
-uint8 S12Pwm_GetOutputState(uint8 ChannelNumber);
-uint8 S12Pwm_GetCounter8(uint8 ChannelNumber);
-uint16 S12Pwm_GetCounter16(uint8 ChannelNumber);
-void S12Pwm_ResetCounter(uint8 ChannelNumber);
+
+/*
+**  Global Functions.
+*/
+void    S12Pwm_Init(S12Pwm_ConfigType const * const ConfigPtr);
+void    S12Pwm_DeInit(void);
+
+void    S12Pwm_SetTimebase(void);
+void    S12Pwm_SetDutyCycle(S12Pwm_ChannelType ChannelNumber, uint16 DutyCycle);
+void    S12Pwm_SetPeriodAndDuty(S12Pwm_ChannelType ChannelNumber, uint16 Period, uint16 DutyCycle);
+
+void    S12Pwm_SetOutputToIdle(S12Pwm_ChannelType ChannelNumber);
+uint8   S12Pwm_GetOutputState(S12Pwm_ChannelType ChannelNumber);
+
+boolean S12Pwm_Is16BitChannel(S12Pwm_ChannelType ChannelNumber);
+
+uint16  S12Pwm_GetCounter(S12Pwm_ChannelType ChannelNumber);
+uint16  S12Pwm_GetPeriod(S12Pwm_ChannelType ChannelNumber);
+uint16  S12Pwm_GetDutyCycle(S12Pwm_ChannelType ChannelNumber);
+uint8 S12Pwm_GetPolarity(S12Pwm_ChannelType ChannelNumber);
+
+void S12Pwm_ActivateChannel(S12Pwm_ChannelType ChannelNumber);
+void S12Pwm_DeactivateChannel(S12Pwm_ChannelType ChannelNumber);
+boolean S12Pwm_ChannelActivated(S12Pwm_ChannelType ChannelNumber);
+
+void S12Pwm_ResetCounter(S12Pwm_ChannelType ChannelNumber);
+
 
 #if defined(__cplusplus)
 }
--- mcu/s12/src/S12_Pwm.c
+++ mcu/s12/src/S12_Pwm.c
@@ -26,91 +26,240 @@
 #include "Hw_Cfg.h"
 
 /*
-** todo:    Functions for 8- and 16-Bit PWM.
+**  Global Variables.
 */
+S12Pwm_ConfigType const * PWM;
 
 /*
 **
 **  REFERENCES:
 **  ===========
-**  AN2612          - PWM GENERATION USING HCS12 TIMER CHANNELS
-**  S12PWM8B8CV1    - HCS12 8-BIT, 8-CHANNEL PULSE WIDTH MODULATOR (PWM) BLOCK GUI
+**  AN2612          - PWM GENERATION USING HCS12 TIMER CHANNELS.
+**  S12PWM8B8CV1    - HCS12 8-BIT, 8-CHANNEL PULSE WIDTH MODULATOR (PWM) BLOCK USER GUIDE.
 **
 */
 
-void S12Pwm_Init(void)
+/*
+**  Global Functions.
+*/
+void S12Pwm_Init(S12Pwm_ConfigType const * const ConfigPtr)
 {
-    S12PWM_REG16(PWMPER01)=PWM.PwmPer01;
-    S12PWM_REG16(PWMPER23)=PWM.PwmPer23;
-    S12PWM_REG16(PWMPER45)=PWM.PwmPer45;
-    S12PWM_REG16(PWMPER67)=PWM.PwmPer67;
 
-    S12PWM_REG16(PWMDTY01)=PWM.PwmDty01;
-    S12PWM_REG16(PWMDTY23)=PWM.PwmDty23;
-    S12PWM_REG16(PWMDTY45)=PWM.PwmDty45;
-    S12PWM_REG16(PWMDTY67)=PWM.PwmDty67;
+    PWM = ConfigPtr;
 
-    S12PWM_REG16(PWMCNT0)=(uint16)0x0000U;
-    S12PWM_REG16(PWMCNT2)=(uint16)0x0000U;
-    S12PWM_REG16(PWMCNT4)=(uint16)0x0000U;
-    S12PWM_REG16(PWMCNT6)=(uint16)0x0000U;
+    /* Setup Initial Periods and Duty-Cycles. */
+    S12PWM_REG16(PWMPER01) = ConfigPtr->PwmPer01;
+    S12PWM_REG16(PWMPER23) = ConfigPtr->PwmPer23;
+    S12PWM_REG16(PWMPER45) = ConfigPtr->PwmPer45;
+    S12PWM_REG16(PWMPER67) = ConfigPtr->PwmPer67;
 
-    S12PWM_REG8(PWMPOL)=PWM.PwmPol;
-    S12PWM_REG8(PWMCLK)=PWM.PwmClk;
-    S12PWM_REG8(PWMPRCLK)=PWM.PwmPrClk;
-    S12PWM_REG8(PWMCAE)=PWM.PwmCae;
-    S12PWM_REG8(PWMSCLA)=PWM.PwmSclA;
-    S12PWM_REG8(PWMSCLB)=PWM.PwmSclB;
+    S12PWM_REG16(PWMDTY01) = ConfigPtr->PwmDty01;
+    S12PWM_REG16(PWMDTY23) = ConfigPtr->PwmDty23;
+    S12PWM_REG16(PWMDTY45) = ConfigPtr->PwmDty45;
+    S12PWM_REG16(PWMDTY67) = ConfigPtr->PwmDty67;
 
-    S12PWM_REG8(PWMSDN)=PWM.PwmSdn;
-    S12PWM_REG8(PWMCTL)=PWM.PwmCtl;
-    S12PWM_REG8(PWME)=PWM.PwmE;         /* OK, start PWM.   */
+    /* Reset Counters. */
+    S12PWM_REG16(PWMCNT0)  = (uint16)0x0000U;
+    S12PWM_REG16(PWMCNT2)  = (uint16)0x0000U;
+    S12PWM_REG16(PWMCNT4)  = (uint16)0x0000U;
+    S12PWM_REG16(PWMCNT6)  = (uint16)0x0000U;
+
+    /* Configure PWM. */
+    S12PWM_REG8(PWMPOL)    = ConfigPtr->PwmPol;
+    S12PWM_REG8(PWMCLK)    = ConfigPtr->PwmClk;
+    S12PWM_REG8(PWMPRCLK)  = ConfigPtr->PwmPrClk;
+    S12PWM_REG8(PWMCAE)    = ConfigPtr->PwmCae;
+    S12PWM_REG8(PWMSCLA)   = ConfigPtr->PwmSclA;
+    S12PWM_REG8(PWMSCLB)   = ConfigPtr->PwmSclB;
+    S12PWM_REG8(PWMSDN)    = ConfigPtr->PwmSdn;
+    S12PWM_REG8(PWMCTL)    = ConfigPtr->PwmCtl;
+
+    /* Enable Channels. */
+    S12PWM_REG8(PWME) = ConfigPtr->PwmE;
 /* prescaler = (E-Clock * 4)/10 */
 
 }
 
 
+//#if defined(S12_PWM_DEINIT_API)
+void S12Pwm_DeInit(void)
+{
+    /* Shutdown all PWM-Channels. */
+    S12PWM_REG8(PWME) = (uint8)0x00;
+
+    /* Reinitialize PortP. */
+    S12PIM_REG8(PTP)       = PWM->IdleState;
+    S12PIM_REG8(DDRP)      = (uint8)0xff;
+}
+//#endif /* HC12_PWM_DEINIT_API */
+
+
 void S12Pwm_SetTimebase(void)
 {
-}
-
-
-void S12Pwm_SetDutyCycle(uint8 ChannelNumber,uint16 DutyCycle)
-{
-}
-
-
-void S12Pwm_SetPeriodAndDuty(uint8 ChannelNumber,uint16 Period,uint16 DutyCycle)
-{
 
 }
 
 
-void S12Pwm_SetOutputToIdle(uint8 ChannelNumber)
+void S12Pwm_SetDutyCycle(S12Pwm_ChannelType ChannelNumber, uint16 DutyCycle)
 {
-
+    if (!S12Pwm_ChannelActivated(ChannelNumber)) {
+       S12Pwm_ActivateChannel(ChannelNumber);
+    }
+    if (S12Pwm_Is16BitChannel(ChannelNumber)) {
+        S12PWM_REG16(PWMDTY0 + (ChannelNumber & (uint8)0x06)) = DutyCycle;
+    } else {
+        S12PWM_REG8(PWMDTY0 + ChannelNumber) = (uint8)DutyCycle;
+    }
 }
 
 
-uint8 S12Pwm_GetOutputState(uint8 ChannelNumber)
+void S12Pwm_SetPeriodAndDuty(S12Pwm_ChannelType ChannelNumber, uint16 Period, uint16 DutyCycle)
 {
-    /* todo: read 'PTIP' */
+    if (!S12Pwm_ChannelActivated(ChannelNumber)) {
+       S12Pwm_ActivateChannel(ChannelNumber);
+    }
+    if (S12Pwm_Is16BitChannel(ChannelNumber)) {
+        S12PWM_REG16(PWMPER0 + (ChannelNumber & (uint8)0x06)) = Period;
+    } else {
+        S12PWM_REG8(PWMPER0 + ChannelNumber) = (uint8)Period;
+    }
+    S12Pwm_SetDutyCycle(ChannelNumber, DutyCycle);
 }
 
 
-void S12Pwm_ResetCounter(uint8 ChannelNumber)
+void S12Pwm_SetOutputToIdle(S12Pwm_ChannelType ChannelNumber)
 {
-    S12PWM_REG8(PWMCNT0+ChannelNumber)=(uint8)0x00;
+    uint8 channel;
+    uint8 mask;
+
+    channel = 1 << ChannelNumber;
+    mask = PWM->IdleState & channel;
+
+    S12PWM_REG8(PTP)    = (S12PWM_REG8(PTP) & ~mask) | mask;
+    S12PWM_REG8(DDRP)    |= channel;
+    S12Pwm_DeactivateChannel(ChannelNumber);
 }
 
 
-uint8 S12Pwm_GetCounter8(uint8 ChannelNumber)
+uint8 S12Pwm_GetOutputState(S12Pwm_ChannelType ChannelNumber)
 {
-    return S12PWM_REG8(PWMCNT0+ChannelNumber);
+    /* read 'PTIP' !? */
+    return (S12Pwm_GetCounter(ChannelNumber) > S12Pwm_GetDutyCycle(ChannelNumber)) ^ S12Pwm_GetPolarity(ChannelNumber);
 }
 
 
-uint16 S12Pwm_GetCounter16(uint8 ChannelNumber)
+uint16 S12Pwm_GetCounter(S12Pwm_ChannelType ChannelNumber)
 {
-    return S12PWM_REG16(PWMCNT0+(ChannelNumber<<1));
+    if (S12Pwm_Is16BitChannel(ChannelNumber)) {
+        return S12PWM_REG16(PWMCNT0 + (ChannelNumber & (uint8)0x06));
+    } else {
+        return (uint16)S12PWM_REG8(PWMCNT0 + ChannelNumber);
+    }
+}
+
+
+uint16 S12Pwm_GetPeriod(S12Pwm_ChannelType ChannelNumber)
+{
+    if (S12Pwm_Is16BitChannel(ChannelNumber)) {
+        return S12PWM_REG16(PWMPER0 + (ChannelNumber & (uint8)0x06));
+    } else {
+        return (uint16)S12PWM_REG8(PWMPER0 + ChannelNumber);
+    }
+}
+
+
+uint16 S12Pwm_GetDutyCycle(S12Pwm_ChannelType ChannelNumber)
+{
+    if (S12Pwm_Is16BitChannel(ChannelNumber)) {
+        return S12PWM_REG16(PWMDTY0 + (ChannelNumber & (uint8)0x06));
+    } else {
+        return (uint16)S12PWM_REG8(PWMDTY0 + ChannelNumber);
+    }
+}
+
+
+uint8 S12Pwm_GetPolarity(S12Pwm_ChannelType ChannelNumber)
+{
+    if (S12Pwm_Is16BitChannel(ChannelNumber)) {
+        ChannelNumber &= (uint8)0x06;
+    }
+
+    return ((S12PWM_REG8(PWMPOL)) & ((uint8)0x01 << ChannelNumber)) >> ChannelNumber;
+}
+
+
+void S12Pwm_ResetCounter(S12Pwm_ChannelType ChannelNumber)
+{
+    if (S12Pwm_Is16BitChannel(ChannelNumber)) {
+        S12PWM_REG16(PWMCNT0 + (ChannelNumber & (uint8)0x06)) = (uint16)0x0000u;
+    } else {
+        S12PWM_REG8(PWMCNT0 + ChannelNumber) = (uint8)0x00;
+    }
+}
+
+
+boolean S12Pwm_Is16BitChannel(S12Pwm_ChannelType ChannelNumber)
+{
+    boolean result = FALSE;
+
+    switch (ChannelNumber) {
+        case 0:
+        case 1:
+
+            if ((S12PWM_REG8(PWMCTL) & CON01) == CON01) {
+                result = TRUE;
+            }
+
+            break;
+        case 2:
+        case 3:
+
+            if ((S12PWM_REG8(PWMCTL) & CON23) == CON23) {
+                result = TRUE;
+            }
+
+            break;
+        case 4:
+        case 5:
+
+            if ((S12PWM_REG8(PWMCTL) & CON45) == CON45) {
+                result = TRUE;
+            }
+
+            break;
+        case 6:
+        case 7:
+
+            if ((S12PWM_REG8(PWMCTL) & CON67) == CON67) {
+                result = TRUE;
+            }
+
+            break;
+        default:
+            ASSERT(FALSE); /* todo: ErrorHandling!!! */
+    }
+
+    return result;
+}
+
+
+void S12Pwm_ActivateChannel(S12Pwm_ChannelType ChannelNumber)
+{
+    S12Pwm_ResetCounter(ChannelNumber);
+    S12PWM_REG8(PWME) |= (1 << ChannelNumber);
+}
+
+
+void S12Pwm_DeactivateChannel(S12Pwm_ChannelType ChannelNumber)
+{
+    S12PWM_REG8(PWME) &= ~(1 << ChannelNumber);
+}
+
+
+boolean S12Pwm_ChannelActivated(S12Pwm_ChannelType ChannelNumber)
+{
+    uint8 channel;
+
+    channel=(1 << ChannelNumber);
+    return (S12PWM_REG8(PWME) & channel)==channel;
 }
