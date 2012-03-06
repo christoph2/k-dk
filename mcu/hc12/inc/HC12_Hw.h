acd0aa75a2841d2428a0d9a767787a11915dfd69
Christoph Schueler <cpu12.gems@googlemail.com>
2012-03-06 17:01:45 +0100

    'HC12_Hw.h": Added include files for controller derivates.

--- mcu/hc12/inc/HC12_Hw.h
+++ mcu/hc12/inc/HC12_Hw.h
@@ -2,7 +2,7 @@
  * k_dk - Driver Kit for k_os (Konnex Operating-System based on the
  * OSEK/VDX-Standard).
  *
- * (C) 2007-2011 by Christoph Schueler <github.com/Christoph2,
+ * (C) 2007-2012 by Christoph Schueler <github.com/Christoph2,
  *                                      cpu12.gems@googlemail.com>
  *
  * All Rights Reserved
@@ -30,21 +30,51 @@
 #include "Sys_Cfg.h"
 #include "InstallISR/ISR.h"
 
-#define HC12_REG8(mod, reg)     (BYTE_REG((mod)->BaseAddr, (reg)))
-#define HC12_REG16(mod, reg)    (WORD_REG((mod)->BaseAddr, (reg)))
 
-#define HC12BCIO_REG8(reg)      (HC12_REG8(&BCIO, (reg)))
-#define HC12BDLC_REG8(reg)      (HC12_REG8(&BDLC, (reg)))
-#define HC12CLK_REG8(reg)       (HC12_REG8(&CLK, (reg)))
+#if CPU_FAMILY == CPU12_HC12
+
+    #if CPU_DERIVATE == CPU12_HC12B32
+        #include "device/hc12b32.h"
+    #elif  CPU_DERIVATE == CPU12_HC12BC32
+        #include "device/hc12bc32.h"
+    #elif  CPU_DERIVATE == CPU12_HC12BD32
+        #include "device/hc12bd32.h"
+    #elif  CPU_DERIVATE == CPU12_HC12A4
+        #include "device/hc12a4.h"
+    #elif  CPU_DERIVATE == CPU12_HC12D60
+        #include "device/hc12d60.h"
+    #elif  CPU_DERIVATE == CPU12_HC12D60A
+        #include "device/hc12d60a.h"
+    #elif  CPU_DERIVATE == CPU12_HC12DG128
+        #include "device/hc12dg128.h"
+    #elif  CPU_DERIVATE == CPU12_HC12DG128A || CPU_DERIVATE == CPU12_HC12DG128C || CPU_DERIVATE == CPU12_HC12DG128P
+        #include "device/hc12dg128a.h"
+    #elif  CPU_DERIVATE == CPU12_HC12DT128A || CPU_DERIVATE == CPU12_HC12DT128C || CPU_DERIVATE == CPU12_HC12DT128P
+        #include "device/hc12dt128a.h"
+    #endif
+
+#else
+    #error  CPU family must be HC12.
+#endif
+
+#define HC12_REG8(mod, reg)     (BYTE_REG(KDK_BASE_ADDRESS(mod), (reg)))
+#define HC12_REG16(mod, reg)    (WORD_REG(KDK_BASE_ADDRESS(mod), (reg)))
+
+#define HC12BCIO_REG8(reg)      (HC12_REG8(BCIO, (reg)))
+#define HC12BDLC_REG8(reg)      (HC12_REG8(BDLC, (reg)))
+
+#define HC12CAN_REG8(base, reg) (BYTE_REG(base, (reg)))
+
+#define HC12CLK_REG8(reg)       (HC12_REG8(CLK, (reg)))
 #define HC12ECT_REG8(reg)       (HC12_REG8(ECT, (reg)))
 #define HC12ECT_REG16(reg)      (HC12_REG16(ECT, (reg)))
-#define HC12EEP_REG8(reg)       (HC12_REG8(&EEP, (reg)))
+#define HC12EEP_REG8(reg)       (HC12_REG8(EEP, (reg)))
 #define HC12INT_REG8(reg)       (HC12_REG8(INT, (reg)))
 #define HC12KWIO_REG8(reg)      (HC12_REG8(KWIO, (reg)))
-#define HC12PLL_REG8(reg)       (HC12_REG8(&PLL, (reg)))
+#define HC12PLL_REG8(reg)       (HC12_REG8(PLL, (reg)))
 #define HC12PWM_REG8(reg)       (HC12_REG8(PWM, (reg)))
 #define HC12PWM_REG16(reg)      (HC12_REG16(PWM, (reg)))
-#define HC12SI_REG8(reg)        (HC12_REG8(&SI, (reg)))
+#define HC12SI_REG8(reg)        (HC12_REG8(SI, (reg)))
+#define HC12SPI_REG8(base, reg) (BYTE_REG(base, (reg)))
 
 #endif /* __HC12_HW_H */
-
