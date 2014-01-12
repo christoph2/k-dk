5e42f32fca7b9bc2bde2aa110cbfbb801cded4b3
Christoph Schueler <cpu12.gems@googlemail.com>
2014-01-12 18:15:25 +0100

    Move 'common/inc' to new location 'inc/kdk/common'.

---
+++ inc/kdk/common/apitracer.h
@@ -0,0 +1,67 @@
+/*
+ * k_dk - Driver Kit for k_os (Konnex Operating-System based on the
+ * OSEK/VDX-Standard).
+ *
+ * (C) 2012 by Christoph Schueler <github.com/Christoph2,
+ *                                      cpu12.gems@googlemail.com>
+ *
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * s. FLOSS-EXCEPTION.txt
+ */
+#if !defined(__APITRACER_H)
+#define __APITRACER_H
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif  /* __cplusplus */
+
+#include "Std_Types.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+**  Global types.
+*/
+typedef struct tagApiTrace_BufferEntryType {
+    uint8 dummy;
+} ApiTrace_BufferEntryType;
+
+typedef enum tagApiTrace_LogTypeType {  /* CHECK: oder besser eine Option um parallel in eine Datei zu loggen? */
+    LOGTYPE_RAM,
+    LOGTYPE_FILE
+} ApiTrace_LogTypeType;
+
+
+/*
+**  Global functions.
+*/
+void ApiTrace_Init(ApiTrace_LogTypeType LogType, uint32 MaxNumEntries);
+void ApiTrace_Finish(void);
+void ApiTrace_Reset(void);
+
+// #define API_TRACE(...) printf(__VA_ARGS__)
+
+#if defined(__cplusplus)
+
+}
+
+#endif  /* __cplusplus */
+
+#endif  /* __APITRACER_H */
