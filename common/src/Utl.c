5413a06f2d1868785f3499b14f7d947f79e3400d
Christoph Schueler <cpu12.gems@googlemail.com>
2014-06-29 12:33:34 +0200

    [Utl.c] Fix data-types.

--- common/src/Utl.c
+++ common/src/Utl.c
@@ -23,54 +23,54 @@
  */
 #include "kdk/common/Utl.h"
 
-const uint8 Utl_SetBitTab8[8] = {
-    (uint8)0x01, (uint8)0x02, (uint8)0x04, (uint8)0x08, (uint8)0x10, (uint8)0x20, (uint8)0x40, (uint8)0x80
+const uint8_t Utl_SetBitTab8[8] = {
+    (uint8_t)0x01, (uint8_t)0x02, (uint8_t)0x04, (uint8_t)0x08, (uint8_t)0x10, (uint8_t)0x20, (uint8_t)0x40, (uint8_t)0x80
 };
 
-const uint8 Utl_ClearBitTab8[8] = {
-    (uint8)0xFE, (uint8)0xFD, (uint8)0xFB, (uint8)0xF7, (uint8)0xEF, (uint8)0xDF, (uint8)0xBF, (uint8)0x7F
+const uint8_t Utl_ClearBitTab8[8] = {
+    (uint8_t)0xFE, (uint8_t)0xFD, (uint8_t)0xFB, (uint8_t)0xF7, (uint8_t)0xEF, (uint8_t)0xDF, (uint8_t)0xBF, (uint8_t)0x7F
 };
 
-const uint16 Utl_SetBitTab16[16] = {
-    (uint16)0x0001U, (uint16)0x0002U, (uint16)0x0004U, (uint16)0x0008U,
-    (uint16)0x0010U, (uint16)0x0020U, (uint16)0x0040U, (uint16)0x0080U,
-    (uint16)0x0100U, (uint16)0x0200U, (uint16)0x0400U, (uint16)0x0800U,
-    (uint16)0x1000U, (uint16)0x2000U, (uint16)0x4000U, (uint16)0x8000U
+const uint16_t Utl_SetBitTab16[16] = {
+    (uint16_t)0x0001U, (uint16_t)0x0002U, (uint16_t)0x0004U, (uint16_t)0x0008U,
+    (uint16_t)0x0010U, (uint16_t)0x0020U, (uint16_t)0x0040U, (uint16_t)0x0080U,
+    (uint16_t)0x0100U, (uint16_t)0x0200U, (uint16_t)0x0400U, (uint16_t)0x0800U,
+    (uint16_t)0x1000U, (uint16_t)0x2000U, (uint16_t)0x4000U, (uint16_t)0x8000U
 };
 
-const uint16 Utl_ClearBitTab16[16] = {
-    (uint16)0xFFFEU, (uint16)0xFFFDU, (uint16)0xFFFBU, (uint16)0xFFF7U,
-    (uint16)0xFFEFU, (uint16)0xFFDFU, (uint16)0xFFBFU, (uint16)0xFF7FU,
-    (uint16)0xFEFFU, (uint16)0xFDFFU, (uint16)0xFBFFU, (uint16)0xF7FFU,
-    (uint16)0xEFFFU, (uint16)0xDFFFU, (uint16)0xBFFFU, (uint16)0x7FFFU
+const uint16_t Utl_ClearBitTab16[16] = {
+    (uint16_t)0xFFFEU, (uint16_t)0xFFFDU, (uint16_t)0xFFFBU, (uint16_t)0xFFF7U,
+    (uint16_t)0xFFEFU, (uint16_t)0xFFDFU, (uint16_t)0xFFBFU, (uint16_t)0xFF7FU,
+    (uint16_t)0xFEFFU, (uint16_t)0xFDFFU, (uint16_t)0xFBFFU, (uint16_t)0xF7FFU,
+    (uint16_t)0xEFFFU, (uint16_t)0xDFFFU, (uint16_t)0xBFFFU, (uint16_t)0x7FFFU
 };
 
 #if 0
-#define BIT_8(n) ((const uint8[8]) {1, 2, 4, 8, 16, 32, 64, 128})[n];
+#define BIT_8(n) ((const uint8_t[8]) {1, 2, 4, 8, 16, 32, 64, 128})[n];
 #endif
 
 static uint32 NextRandomNumber = 1;
 
 #if 0
-boolean Utl_BitGet(uint16 w, uint8 num)
+boolean Utl_BitGet(uint16_t w, uint8_t num)
 {
-    return (w & Utl_SetBitTab16[num]) != (uint16)0x0000U;
+    return (w & Utl_SetBitTab16[num]) != (uint16_t)0x0000U;
 }
 
 
-uint16 Utl_BitSet(uint16 w, uint8 num)
+uint16_t Utl_BitSet(uint16_t w, uint8_t num)
 {
     return w |= Utl_SetBitTab16[num];
 }
 
 
-uint16 Utl_BitReset(uint16 w, uint8 num)
+uint16_t Utl_BitReset(uint16_t w, uint8_t num)
 {
     return w &= ~(Utl_SetBitTab16[num]);
 }
 
 
-uint16 Utl_BitToggle(uint16 w, uint8 num)
+uint16_t Utl_BitToggle(uint16_t w, uint8_t num)
 {
     return w ^= Utl_SetBitTab16[num];
 }
@@ -78,38 +78,38 @@ uint16 Utl_BitToggle(uint16 w, uint8 num)
 
 #endif
 
-uint16 Utl_BitGetHighest(uint16 w)
+uint16_t Utl_BitGetHighest(uint16_t w)
 {
     w |= w >> 1;
     w |= w >> 2;
     w |= w >> 4;
     w |= w >> 8;
 
-    return (uint16)(w ^ (w >> 1));
+    return (uint16_t)(w ^ (w >> 1));
 }
 
 
-uint16 Utl_BitGetLowest(uint16 w)
+uint16_t Utl_BitGetLowest(uint16_t w)
 {
-    return (uint16)((~w + 1) & w);
+    return (uint16_t)((~w + 1) & w);
 }
 
 
-uint16 Utl_BitSetLowest(uint16 w)
+uint16_t Utl_BitSetLowest(uint16_t w)
 {
-    return (uint16)(w | (w + 1));
+    return (uint16_t)(w | (w + 1));
 }
 
 
-uint16 Utl_BitResetLowest(uint16 w)
+uint16_t Utl_BitResetLowest(uint16_t w)
 {
-    return (uint16)(w & (w - 1));
+    return (uint16_t)(w & (w - 1));
 }
 
 
-uint8 Utl_Log2(uint16 num)
+uint8_t Utl_Log2(uint16_t num)
 {
-    uint8 res = (uint8)0x00;
+    uint8_t res = (uint8_t)0x00;
 
     while (num >>= 1) {
         res++;
@@ -121,10 +121,10 @@ uint8 Utl_Log2(uint16 num)
 
 Utl_EndianessType Utl_CheckEndianess(void)
 {
-    const uint16    foo    = 0xaa55U;
-    uint8 *         ptr    = (uint8 *)&foo;
+    const uint16_t    foo    = 0xaa55U;
+    uint8_t *         ptr    = (uint8_t *)&foo;
 
-    if (0[ptr] == (uint8)0xaa) {
+    if (0[ptr] == (uint8_t)0xaa) {
         return UTL_BIG_ENDIAN;
     } else {
         return UTL_LITTLE_ENDIAN;
@@ -134,8 +134,8 @@ Utl_EndianessType Utl_CheckEndianess(void)
 
 void Utl_MemCopy(void * RESTRICT dst, void * RESTRICT src, SizeType len)
 {
-    uint8 * pd = (uint8 *)dst;
-    uint8 * ps = (uint8 *)src;
+    uint8_t * pd = (uint8_t *)dst;
+    uint8_t * ps = (uint8_t *)src;
 
     ASSERT(dst != (void *)NULL);
     ASSERT(pd >= ps + len || ps >= pd + len);
@@ -148,9 +148,9 @@ void Utl_MemCopy(void * RESTRICT dst, void * RESTRICT src, SizeType len)
 }
 
 
-void Utl_MemSet(void * dest, uint8 fill_char, SizeType len)
+void Utl_MemSet(void * dest, uint8_t fill_char, SizeType len)
 {
-    uint8 * p = (uint8 *)dest;
+    uint8_t * p = (uint8_t *)dest;
 
     ASSERT(dest != (void *)NULL);
 
@@ -160,25 +160,25 @@ void Utl_MemSet(void * dest, uint8 fill_char, SizeType len)
 }
 
 
-void Utl_Randomize(uint16 seed)
+void Utl_Randomize(uint16_t seed)
 {
     NextRandomNumber = (uint32)seed;
 }
 
 
-uint16 Utl_Random(void)
+uint16_t Utl_Random(void)
 {
     NextRandomNumber = NextRandomNumber * (uint32)0x41C64E6DUL + (uint32)0x3039UL;
-    return (uint16)(NextRandomNumber / (uint32)0x10000UL) % (uint32)0x8000UL;
+    return (uint16_t)(NextRandomNumber / (uint32)0x10000UL) % (uint32)0x8000UL;
 
 }
 
 
-uint16 Utl_Sqrt16(uint16 x)
+uint16_t Utl_Sqrt16(uint16_t x)
 {
-   uint16 res = 1U;
-   uint16 sqr = 1U;
-   uint16 delta = 3U;
+   uint16_t res = 1U;
+   uint16_t sqr = 1U;
+   uint16_t delta = 3U;
 
    while (sqr <= x) {
       res += 1U;
@@ -204,7 +204,7 @@ uint32 Utl_Sqrt32(uint32 x)
 }
 
 
-SizeType Utl_StrLen(const uint8 * src)
+SizeType Utl_StrLen(const uint8_t * src)
 {
     SizeType len = (SizeType)0x0000;
 
@@ -216,7 +216,7 @@ SizeType Utl_StrLen(const uint8 * src)
 }
 
 
-void Utl_StrCat(uint8 * RESTRICT dst, const uint8 * RESTRICT src)
+void Utl_StrCat(uint8_t * RESTRICT dst, const uint8_t * RESTRICT src)
 {
     ASSERT(dst != (void *)NULL);
 
@@ -230,7 +230,7 @@ void Utl_StrCat(uint8 * RESTRICT dst, const uint8 * RESTRICT src)
 }
 
 
-void Utl_StrCpy(uint8 * RESTRICT dst, const uint8 * RESTRICT src)
+void Utl_StrCpy(uint8_t * RESTRICT dst, const uint8_t * RESTRICT src)
 {
     ASSERT(dst != (void *)NULL);
 
@@ -240,7 +240,7 @@ void Utl_StrCpy(uint8 * RESTRICT dst, const uint8 * RESTRICT src)
 
 
 #if 0
-void Utl_StrNCpy(uint8 * RESTRICT dst, const uint8 * RESTRICT src, SizeType maxlen)
+void Utl_StrNCpy(uint8_t * RESTRICT dst, const uint8_t * RESTRICT src, SizeType maxlen)
 {
     ASSERT(dest != (void *)NULL);
 
@@ -251,11 +251,11 @@ void Utl_StrNCpy(uint8 * RESTRICT dst, const uint8 * RESTRICT src, SizeType maxl
 
 #endif
 
-void Utl_StrRev(uint8 * str)
+void Utl_StrRev(uint8_t * str)
 {
     SizeType    idx;
     SizeType    dpos;
-    uint8       ch;
+    uint8_t       ch;
 
     ASSERT(str != (void *)NULL);
 
@@ -267,7 +267,7 @@ void Utl_StrRev(uint8 * str)
 }
 
 
-const uint8 * Utl_StrChr(const uint8 * str, uint8 ch)
+const uint8_t * Utl_StrChr(const uint8_t * str, uint8_t ch)
 {
     ASSERT(str != (void *)NULL);
 
@@ -277,19 +277,19 @@ const uint8 * Utl_StrChr(const uint8 * str, uint8 ch)
         }
     }
 
-    return (const uint8 *)NULL;
+    return (const uint8_t *)NULL;
 }
 
 
-void Utl_Itoa(sint32 value, uint8 base, uint8 * buf)
+void Utl_Itoa(sint32 value, uint8_t base, uint8_t * buf)
 {
     uint32  mod;
-    uint8   pos = (uint8)0x00, swap_pos = (uint8)0x00;
-    uint8   ch;
+    uint8_t   pos = (uint8_t)0x00, swap_pos = (uint8_t)0x00;
+    uint8_t   ch;
 
     ASSERT(buf != (void *)NULL);
 
-    if (((sint32)value) < 0L && base == (uint8)10) {
+    if (((sint32)value) < 0L && base == (uint8_t)10) {
         value      = (uint32)((sint32)value * -1L);
         buf[0]     = '-';
         swap_pos   = 1;
@@ -306,7 +306,7 @@ void Utl_Itoa(sint32 value, uint8 base, uint8 * buf)
         if (mod < 10) {
             buf[pos++] = '0' + mod;
         } else {
-            buf[pos++] = 'A' + mod - (uint8)10;
+            buf[pos++] = 'A' + mod - (uint8_t)10;
         }
 
         value /= base;
@@ -325,25 +325,25 @@ void Utl_Itoa(sint32 value, uint8 base, uint8 * buf)
 
 
 #if 0
-void const * Utl_BinSearch(void const * key, void const * base, uint16 num_elems, uint16 elem_size,
+void const * Utl_BinSearch(void const * key, void const * base, uint16_t num_elems, uint16_t elem_size,
                            Utl_CompareFuncType compare_func)
 {
-    uint16  mid;
+    uint16_t  mid;
     sint16  left, right;
-    uint8 * cmp_key;
+    uint8_t * cmp_key;
     sint8   res;
 
     left   = (sint16)0x0000;
     right  = (sint16)(num_elems - 1);
 
     do {
-        mid        = (uint16)(left + right) >> 1;
-        cmp_key    = (uint8 *)((uint8 *)base + (mid * elem_size));
+        mid        = (uint16_t)(left + right) >> 1;
+        cmp_key    = (uint8_t *)((uint8_t *)base + (mid * elem_size));
         res        = compare_func(key, cmp_key);
 
         if (res == (sint16)0) {
             return cmp_key;
-        } else if (res < (uint8)0) {
+        } else if (res < (uint8_t)0) {
             right = mid - 1;
         } else {
             left = mid + 1;
@@ -382,7 +382,7 @@ boolean Utl_IsNull(void * Ptr)
 }
 
 
-uint16 Utl_Swap16(uint16 * w)
+uint16_t Utl_Swap16(uint16_t * w)
 {
     return MAKEWORD(LOBYTE(*w), HIBYTE(*w));
 }
